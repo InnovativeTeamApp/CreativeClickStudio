78d51dbff8bc77dd6970ad128db952a3fe4f2a1f
rajeshmine <rajeshjas20296@gmail.com>
2019-11-29 00:52:06 +0530

    CCUI-1 Initialize Project Folder

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './containers/Home/Home';
+
+class App extends Component {
+    render() {
+        return (
+                <Switch>
+                    <Route path="/" component={Home} />
+                </Switch>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// import registerServiceWorker from './registerServiceWorker';
+
+const app = (
+    <Router>
+        <App />
+    </Router>
+);
+
+
+ReactDOM.render(app, document.getElementById('root'));
+// registerServiceWorker();
